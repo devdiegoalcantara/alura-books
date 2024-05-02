b4f1c66cca01a637e7e6b9ec704b2a917259971e
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-05-02 15:27:46 -0300

    filter

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,9 +1,13 @@
+const elementoParaInserirLivros = document.getElementById('livros')
+
 function exibirOsLivrosNaTela(listaDeLivros) {
-  elementoParaInserirLivros.innerHTML = ''
+    elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        // let disponibilidade = verificarDisponibilidadeDoLivro(livro)
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel' 
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}"
+        <img class="${disponibilidade}" src="${livro.imagem}"
           alt="${livro.alt}" />
         <h2 class="livro__titulo">
           ${livro.titulo}
@@ -17,3 +21,11 @@ function exibirOsLivrosNaTela(listaDeLivros) {
         `
     })
 }
+
+// function verificarDisponibilidadeDoLivro(livro) {
+//   if (livro.quantidade > 0 ){
+//     return 'livro__imagens'
+//   } else {
+//     return 'livros__imagens indisponivel'
+//   }
+// }
