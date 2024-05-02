81fa56820c7b2b9a9d3b54ac07e3e644474bb003
Diego Alcântara <108950234+devdiegoalcantara@users.noreply.github.com>
2024-05-02 15:35:16 -0300

    js

--- app/main.js
+++ app/main.js
@@ -1,11 +1,10 @@
 let livros = []
-const endPointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
 getBuscarLivrosDaAPI()
-const elementoParaInserirLivros = document.getElementById('livros')
 
 async function getBuscarLivrosDaAPI() {
-    const res = await fetch(endPointDaAPI)
+    const res = await fetch(endpointDaAPI)
     livros = await res.json()
     livros = aplicarDesconto(livros)
-    exibirOsLivrosNaTela(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,3 +1,4 @@
+
 const botoes = document.querySelectorAll('.btn')
 botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
